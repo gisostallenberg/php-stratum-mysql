f01eed496095997ae41c6b0421911edd386924b9
Paul Water <p.r.water@setbased.nl>
2019-08-17 00:14:19 +0200

    Fixed test issues.

---
+++ test/Application/StratumApplicationTest.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace SetBased\Stratum\MySql\Test\Application;
+
+use PHPUnit\Framework\TestCase;
+use SetBased\Stratum\Application\Stratum;
+use Symfony\Component\Console\Tester\ApplicationTester;
+
+/**
+ * Test cases for the stratum application.
+ */
+class StratumApplicationTest extends TestCase
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  public function testExecute()
+  {
+    $application = new Stratum();
+    $application->setAutoExit(false);
+
+    $tester = new ApplicationTester($application);
+    $tester->run(['command'     => 'stratum',
+                  'config file' => 'test/etc/stratum.ini']);
+
+    self::assertSame(0, $tester->getStatusCode(), $tester->getDisplay());
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------------------------------------------------------------------------------
