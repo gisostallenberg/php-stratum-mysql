31da92d9a16104b78651e8a36d8eb980c34166b5
Paul Water <p.r.water@setbased.nl>
2020-12-11 10:12:41 +0100

    Restart mysqld instead of mysql.

--- test/MySqlDataLayerTest.php
+++ test/MySqlDataLayerTest.php
@@ -149,7 +149,7 @@ class MySqlDataLayerTest extends DataLayerTestCase
     self::assertTrue(true);
 
     // Reconnect when server has been gone.
-    exec('sudo systemctl restart mysql > /dev/null 2>&1 || sudo service mysql restart');
+    exec('sudo systemctl restart mysqld > /dev/null 2>&1 || sudo service mysql restart');
     $dl->connectIfNotAlive();
     self::assertTrue(true);
 
